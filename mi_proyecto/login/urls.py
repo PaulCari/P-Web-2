253f4bce6a8df14a18d0d37819d17e0304d24cd5
PaulCari <pcaril@unsa.edu.pe>
2024-06-04 13:33:29 -0500

    url

--- mi_proyecto/login/urls.py
+++ mi_proyecto/login/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', include('core.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
