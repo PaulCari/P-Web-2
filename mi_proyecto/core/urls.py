ba47e4f2b337ac892a312a323eaa46313686ecda
PaulCari <pcaril@unsa.edu.pe>
2024-06-04 13:31:32 -0500

    Añadimos la url de logout

--- mi_proyecto/core/urls.py
+++ mi_proyecto/core/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 
 from django.urls import path
 from .views import home, products, register
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
     path('', home, name='home'),
     path('products/', products, name='products'),
     path('register/', register, name='register'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
